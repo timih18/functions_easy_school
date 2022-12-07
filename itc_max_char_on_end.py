0d3fd216c0e38e151d455a7eb6250c9032f7ff05
timih18 <116382351+timih18@users.noreply.github.com>
2022-12-07 22:37:28 +0300

    Add files via upload

--- itc_max_char_on_end.py
+++ itc_max_char_on_end.py
@@ -1,13 +1,15 @@
 def itc_max_char_on_end(str):
+    a = '1234567890'
     len = 0
     max = 0
     for i in str:
         len += 1
     for i in range(len):
-        cnt = 1
-        for z in range(i, len-1):
-            if str[i] == str[z]:
-                cnt += 1
-        if cnt > max:
-            max = cnt
-        return max
+        if str[i] in a:
+            cnt = 1
+            for z in range(i, len-1):
+                if str[i] == str[z]:
+                    cnt += 1
+            if cnt > max:
+                max = cnt
+    return max
