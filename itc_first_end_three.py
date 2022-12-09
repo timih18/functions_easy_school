32c800b67c2c9195866c1e0d6593f9418686c843
timih18 <116382351+timih18@users.noreply.github.com>
2022-12-09 19:16:40 +0300

    Delete itc_first_end_three.py

--- itc_first_end_three.py
+++
@@ -1,8 +0,0 @@
-def itc_first_end_three(str):
-    len = 0
-    for i in str:
-        len += 1
-    if len > 5:
-        print(str[0], str[1], str[2], str[-3], str[-2], str[-1], sep='')
-    else:
-        print(str[0])
