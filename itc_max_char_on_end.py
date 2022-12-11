d665a7f15e5c652a0070ae9fd723edc6f2ed916f
timih18 <116382351+timih18@users.noreply.github.com>
2022-12-11 19:31:10 +0300

    Add files via upload

---
+++ itc_max_char_on_end.py
@@ -0,0 +1,14 @@
+def itc_max_char_on_end(string):
+    cnt = 0
+    len = 0
+    cnt_max = 0
+    for i in string:
+        len +=1
+    for i in range(len):
+        if string[i] >= '0' and string[i] <= '9':
+            cnt += 1
+            if cnt > cnt_max:
+                cnt_max = cnt
+        else:
+            cnt = 0
+    return cnt_max
